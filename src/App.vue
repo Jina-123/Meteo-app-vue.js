3622a533e43149fe88739cf9793668749bcfc2fe
Jinhee Yang <78382611+Jina-123@users.noreply.github.com>
2021-08-24 22:32:42 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"> 
+    <meteo></meteo>
+  </div>
+</template>
+
+<script>
+import Meteo from './components/Meteo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'meteo': Meteo
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url('./assets/fondecran.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueMoment from 'vue-moment'
+
+Vue.use(VueMoment);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
